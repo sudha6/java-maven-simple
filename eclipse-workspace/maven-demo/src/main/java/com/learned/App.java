57563642f1f75c4cf925deb527c76ab6c4aca95a
sudha6 <sudhamadhu1100@gmail.com>
2019-12-23 18:50:37 +0530

    Simple Maven project file added

---
+++ eclipse-workspace/maven-demo/src/main/java/com/learned/App.java
@@ -0,0 +1,13 @@
+package com.learned;
+
+/**
+ * sample maven project!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "First Maven Project created!" );
+    }
+}
